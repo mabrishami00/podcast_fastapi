3e82fadb5b5afaae0253440ba84c866c3ff57657
mabrishami00 <mabrishami78@yahoo.com>
2023-11-10 08:34:23 +0330

    feat: add subscribes interactions

--- app/api/api_v1/endpoints/interaction.py
+++ app/api/api_v1/endpoints/interaction.py
@@ -109,3 +109,70 @@ async def comment_list(channel_id: int, item_id: int, authorization=Header(defau
     else:
         return JSONResponse({"detail": "You are not authenticated."})
     
+
+@router.post("/subscribe/{channel_id}/")
+async def subscribe(channel_id: int, authorization=Header(default=None)):
+    username, jti = jwt_authentication.authenticate(authorization, settings.SECRET_KEY)
+    if username:
+        async with httpx.AsyncClient() as client:
+            url = f"{settings.DOES_CHANNEL_EXIST_URL}/{channel_id}/"
+            response = await client.get(url=url)
+            if response.status_code == 200:
+                value = {
+                    "username": username,
+                    "channel_id": channel_id,
+                }
+                actioned = await subscribes_collection.find_one(value)
+                if actioned:
+                    return JSONResponse(
+                        {"detail": f"You have subscribed this item before."}
+                    )
+                else:
+                    await subscribes_collection.insert_one(value)
+                    return JSONResponse(
+                        {"detail": f"You subscribed this item successfully."}
+                    )
+            else:
+                return JSONResponse({"detail": "Item has not been found."})
+
+    else:
+        return JSONResponse({"detail": "You are not authenticated."})
+
+
+@router.post("/unsubscribe/{channel_id}/")
+async def unsubscribe(channel_id: int, authorization=Header(default=None)):
+    username, jti = jwt_authentication.authenticate(authorization, settings.SECRET_KEY)
+    if username:
+        async with httpx.AsyncClient() as client:
+            url = f"{settings.DOES_CHANNEL_EXIST_URL}/{channel_id}/"
+            response = await client.get(url=url)
+            if response.status_code == 200:
+                value = {
+                    "username": username,
+                    "channel_id": channel_id,
+                }
+                liked = await subscribes_collection.find_one(value)
+                if not liked:
+                    return JSONResponse(
+                        {"detail": f"You have not subscribed this item before."}
+                    )
+                else:
+                    subscribes_collection.delete_one(value)
+                    return JSONResponse(
+                        {"detail": f"You unsubscribed this item successfully."}
+                    )
+            else:
+                return JSONResponse({"detail": "Item has not been found."})
+    else:
+        return JSONResponse({"detail": "You are not authenticated."})
+
+
+@router.get("/subscribe_list/")
+async def subscribe_list(authorization=Header(default=None)):
+    username, jti = jwt_authentication.authenticate(authorization, settings.SECRET_KEY)
+    if username:
+        results = subscribes_collection.find(
+            {"username": username}, projection={"_id": False, "username": False}
+        )
+        documents = [document for document in await results.to_list(length=100)]
+        return JSONResponse(documents)
